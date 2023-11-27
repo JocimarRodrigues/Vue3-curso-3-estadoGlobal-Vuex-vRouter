186d9273800a9b75eba4587f70aec7653a80377a
John Rodrigues <jocimarrodrigues1337@gmail.com>
2023-11-27 17:19:16 -0300

    Criando componente de notificacao

--- aula-04-notificando-usuarios/src/App.vue
+++ aula-04-notificando-usuarios/src/App.vue
@@ -7,6 +7,7 @@
     </div>
     <div class="column is-three-quarter conteudo">
       <!--Aqui vai a viw correspondente-->
+      <Notificacoes />
       <RouterView></RouterView>
     </div>
 
@@ -16,12 +17,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
+import Notificacoes from './components/Notificacoes.vue';
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-
-  },
+    Notificacoes
+},
   data() {
     return {
       modoEscuroAtivo: false
---
+++ aula-04-notificando-usuarios/src/components/Notificacoes.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="notificacoes">
+        <article class="message is-success">
+            <div class="message-header">
+                <div class="message-body">
+                    Aqui vai o texto da notificação
+                </div>
+            </div>
+        </article>
+        <article class="message is-warning">
+            <div class="message-header">
+                <div class="message-body">
+                    Aqui vai o texto da notificação
+                </div>
+            </div>
+        </article>
+        <article class="message is-danger">
+            <div class="message-header">
+                <div class="message-body">
+                    Aqui vai o texto da notificação
+                </div>
+            </div>
+        </article>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'NotificacoesComponent'
+})
+</script>
+
+<style scoped>
+.notificacoes {
+    position: absolute;
+    right: 0;
+    widows: 300px;
+    padding: 8px;
+    z-index: 105;
+}
+</style>
